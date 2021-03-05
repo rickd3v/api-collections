98fe0d80351780cac27d65ead3439a50ccb40e58
Felipe Henrique <fheehenrique@gmail.com>
2021-03-04 23:00:12 -0300

    created double array test class

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,21 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 1500.0
+    salarios[1] = 1000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+
+    println("---------------------")
+    salarios.forEachIndexed {index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("---------------------")
+    val salarios2 = doubleArrayOf(2000.0, 5000.0, 1300.5)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+}
